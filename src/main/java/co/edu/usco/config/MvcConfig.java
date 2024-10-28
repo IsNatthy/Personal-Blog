c6000e7bcbc743f0ae4715bec40eb7989cf9df2f
Nathalia Avila <nathyavila05@gmail.com>
2024-10-27 21:44:11 -0500

    Creación de vista para creación de un post y sus respectivos endpoints.

---
+++ src/main/java/co/edu/usco/config/MvcConfig.java
@@ -0,0 +1,19 @@
+package co.edu.usco.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private UserInterceptor userInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userInterceptor);
+    }
+
+}
--- src/main/java/co/edu/usco/controller/PostController.java
+++ src/main/java/co/edu/usco/controller/PostController.java
@@ -1,12 +1,18 @@
 package co.edu.usco.controller;
 
+import co.edu.usco.entity.PostEntity;
+import co.edu.usco.entity.UserEntity;
 import co.edu.usco.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import co.edu.usco.service.PostService;
 import org.springframework.ui.Model;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/posts")
@@ -18,10 +24,27 @@ public class PostController {
     @Autowired
     private UserService userService;
 
+
     @GetMapping("/home")
     public String homePage(Model model) {
         model.addAttribute("posts", postService.getAllPosts());
         return "/posts/home";
+    }
+
+    @GetMapping("/new")
+    public String newPostPage() {
+        return "/posts/create-post";
+    }
+
+    @PostMapping("/create")
+    public String createPost(PostEntity post, HttpSession session) {
+        post.setCreatedAt(LocalDateTime.now());
+
+        UserEntity user = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString())).get();
+        post.setUser(user);
+
+        postService.createPost(post);
+        return "redirect:/posts/home";
     }
 
 }
